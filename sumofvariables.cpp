03c82b335d4a916cc748624c8a74aa1cd15a2958
Rishivarade <varadehrushikesh87@mail.com>
2025-01-31 12:52:45 +0530

    SUM

---
+++ sumofvariables.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+//swap of variables using third variable:-
+//int main() {
+//	
+//    int num1 = 10;
+//    int num2 = 20;
+//    int temp = num1;
+//    num1=num2;
+//    num2=temp;
+//    cout<<num1<<endl;
+//    cout<<num2<<endl;
+//    return 0;
+//}
+
+
+//swap of variables without third variable:-
+//int main(){
+//	int a = 10;
+//	int b = 20;
+//	
+//	a=a+b;
+//	b=a-b;
+//	a=a-b;
+//	
+//	cout<<a<<endl;
+//	cout<<b<<endl;
+//	
+//	return 0;
+//}
+
+
+//swap of variables without third variable ,+ and -. :-
+//int main(){
+//	int a = 10;
+//	int b = 20;
+//	
+//	a = a * b;
+//  b = a / b;
+//  a = a / b;
+//    
+//  cout<<a<<endl;
+//	cout<<b<<endl;
+//
+//	return 0;
+//}  
+
+//swap of variables without third variable ,+ ,-,*,/. :-
+//int main(){
+//	int a = 10;
+//	int b = 20;
+//	
+//    a = a ^ b;  
+//    b = a ^ b;  
+//    a = a ^ b;
+//    
+//    cout<<"a ="<<a<<endl;
+//	cout<<"b ="<<b<<endl;
+//	
+//	
+//	
+//	return 0;
+//}
+
+
+
