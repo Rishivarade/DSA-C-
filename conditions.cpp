204bfd36748639d47515d17aebf985841d7cd4ab
Rishivarade <varadehrushikesh87@mail.com>
2025-02-01 14:41:19 +0530

    condition

---
+++ conditions.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+using namespace std;
+
+// Q.1
+// WAP TO CHECK THE VOTING ELIGIBILITY OF USER
+
+// int main() {
+//     int age;
+
+//     cout << "Enter your age: ";
+//     cin >> age;
+
+//     if (age < 1 || age > 100) {
+//         cout << "Please enter a valid age between 1 and 100." << endl;
+//     }
+//     else if (age >= 18) {
+//         cout << "You are eligible to vote." << endl;
+//     } 
+//     else if (age < 18){
+//         cout << "You are not eligible";
+//     }
+//     else {
+//         cout << "You are not eligible to vote." << endl;
+//     }
+
+//     return 0;
+// }
+
+//  output:- 
+//  Enter your age: 25
+//  You are eligible to vote.
+
+
+// Q.2
+// WAP TO CHECK GIVEN NUMBER IS DIVISIBLE BY 7 OR NOT.
+
+// int main() {
+//     int num;
+
+//     cout << "Enter a number: ";
+//     cin >> num;
+
+    
+//     if (num % 7 == 0) {
+//         cout << num << " is divisible by 7." << endl;
+//     } else {
+//         cout << num << " is not divisible by 7." << endl;
+//     }
+
+//     return 0;
+// }
+
+// output:- 
+// Enter a number: 14
+// 14 is divisible by 7.
+
+
+// Q.3
+// WAP TO CHECK NUMBER IS NEGATIVE, POSITIVE, OR NEUTRAL.
+
+// int main() {
+//     int num;
+
+//     cout << "Enter a number: ";
+//     cin >> num;
+
+   
+//     if (num > 0) {
+//         cout << num << " is a positive number." << endl;
+//     } else if (num < 0) {
+//         cout << num << " is a negative number." << endl;
+//     } else {
+//         cout << "The number is neutral (zero)." << endl;
+//     }
+
+//     return 0;
+// }
+
+// output:-
+// Enter a number: 10
+// 10 is a positive number.
+
+// Q.4
+// WAP TO PRINT GRADES OF STUDENT BASED ON THEIR PERCENTAGE. 
+
+// int main() {
+
+//     float percentage;
+
+   
+//     cout << "Enter your percentage: ";
+//     cin >> percentage;
+
+   
+//     if (percentage >= 90) {
+//         cout << "Grade: A+" << endl;
+//     } 
+//     else if (percentage >= 80) {
+//         cout << "Grade: A" << endl;
+//     } 
+//     else if (percentage >= 70) {
+//         cout << "Grade: B" << endl;
+//     } 
+//     else if (percentage >= 60) {
+//         cout << "Grade: C" << endl;
+//     } 
+//     else if (percentage >= 50) {
+//         cout << "Grade: D" << endl;
+//     } 
+//     else if (percentage >= 35) {
+//         cout << "Grade: E" << endl;
+//     } 
+//     else {
+//         cout << "Grade: F (Fail)" << endl;
+//     }
+
+//     return 0;
+// }
+
+// output:- 
+// Enter your percentage: 54.12
+// Grade: D
+
+
+
+
+
+
+
